489aea49f9e314f7a91ccac8f293e673e6330894
Steven Fischer <swcfischer@gmail.com>
2018-05-04 15:19:07 -0500

    many changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => (
     <h1>Overview of Application</h1>
 
     <p>This application makes use of the api provided by The Movie Database.</p>
-    <p>If you click the "Explore" link above, you can view an array of popular movies. You can click "next page" or click the exact page your would like to see.
+    <p>If you click the "Explore" link above, you can view an array of popular movies. You can click "next" or click the exact page your would like to see.
     If you click a movie, you will be taken to a component that showcases the movie.
     </p>
     <h2>Search</h2>
@@ -19,7 +19,7 @@ const Home = () => (
     <h2>React Libaries</h2>
     <p>
       I made use of a React library called Overdrive. It provides the animation that makes each movie image travel to its future spot when you click to view its details.
-      I used the 'styled-components' for my styling, and created my own Backbutton component, which pass this.context.router.history.goBack() to an onClick event to return to the previous page.
+      I used the 'styled-components' for my styling, and created my own Backbutton component, which passes this.context.router.history.goBack() to an onClick event to return to the previous page.
     </p>
     <p>If you would like to contact me, please visit my portfolio website: <Link style={{ color: '#000', fontWeight: 'bold', textDecoration: 'underline' }} to="http://stevefischer.me/" target="_blank">Here</Link></p>
   </HomeContainer>
