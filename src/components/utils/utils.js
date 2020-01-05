44cc4350ea51a5bdb693dd0381205be51ba14f8c
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2020-01-05 15:11:59 -0600

    fixed config

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,7 +1,9 @@
+import keys from '../../config/keys';
+
 const api = {
   async fetchPage(page) {
     const results = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=49ab04c3e99d5e8468550f88238d2d2f&language=en-US&certification.lte=G&sort_by=popularity.desc&include_adult=false&include_video=true&page=${page}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${keys.apiKey}&language=en-US&certification.lte=G&sort_by=popularity.desc&include_adult=false&include_video=true&page=${page}`
     );
     const movies = await results.json();
 
@@ -10,7 +12,7 @@ const api = {
 
   async fetchMovieDetail(movieId) {
     const res = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=49ab04c3e99d5e8468550f88238d2d2f&language=en-US`
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${keys.apiKey}&language=en-US`
     );
     const movie = await res.json();
     return movie;
